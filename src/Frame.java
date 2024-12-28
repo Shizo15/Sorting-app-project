d718b8f144712e308f172474b93e0ca2d4060129
Shizo15 <damianszymczyk153@gmail.com>
2024-12-28 23:06:37 +0100

    feat: created sorting functions and fixed problems with data types

--- src/Frame.java
+++ src/Frame.java
@@ -1,9 +1,6 @@
 import javax.swing.*;
-import javax.swing.plaf.InsetsUIResource;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -140,6 +137,8 @@ public class Frame extends JFrame {
             JOptionPane.showMessageDialog(this, "Dane zostały wygenerowane!");
 
         });
+        //dodać zmianę wartości w comboBoxie zaleznie od danych zawartych w wczytywanym pliku
+        //dodac sortowanie w try catch i zrobić komunikat o niepoprawnie wybranym typie danych
 
         //Wczytaj plik
         loadFileButton.addActionListener(e -> {
@@ -150,13 +149,133 @@ public class Frame extends JFrame {
                 while ((line = br.readLine())!=null){
                     data.add(line);
                 }
-                dataArea.setText(String.join("\n", data));
+                dataArea.setText(String.join("\n", data).replace(',','.'));
                 JOptionPane.showMessageDialog(this, "Dane zostały wczytane pomyślnie!");
             } catch (IOException ex) {
                 JOptionPane.showMessageDialog(this, "Błąd podczas wczytywania pliku. Spróbuj ponownie!");
             }
         });
+
+        //Sortuj
+        //tutaj trzeba zrobić tą metodę która rozpozanje typ danych w pliku z którego importujemy dane
+        //tutaj trzeba też zrobić automatyczną zmianę opcji w comboboxie z wyborem typu danych
+        // 0 int, 1 double, 2 string
+        //dataTypeComboBox.setSelectedItem(dataTypes[0]);
+
+        //pobieramy pierwszą linię
+        //robimy try catch sprawdzjąc kolejno czy int, jak nie to czy double, jak nie to na pewno string
+
+
+        sortButton.addActionListener(e -> {
+            if(dataArea.getText().isEmpty()){
+                JOptionPane.showMessageDialog(this, "Brak danych do posortowania!");
+            }
+
+            //
+            try{
+                System.out.println("Blok 1");
+                String text = dataArea.getText();
+                System.out.println(text);
+                String firstValue = text.split("\n")[0];
+                System.out.println("Pierwsza wartość: "+firstValue);
+
+                if(tryParseInt(firstValue)){
+                    dataTypeComboBox.setSelectedItem(dataTypes[0]);
+                }
+                else if (tryParseDouble(firstValue)) {
+                    dataTypeComboBox.setSelectedItem(dataTypes[1]);
+                }
+                else {
+                    dataTypeComboBox.setSelectedItem(dataTypes[2]);
+                }
+            }
+            catch (NumberFormatException ex){
+                JOptionPane.showMessageDialog(this,"Coś dziwnego się stało z formatem danych");
+            }
+            //
+
+            String type = (String) dataTypeComboBox.getSelectedItem();
+            if(type.equals("Liczby całkowite")){
+                //stworzyć liste
+                //przepisać zawartość textArea do listy
+                //wywołanie metody
+                //czyścimy pole textArea żeby wpisać do niego posortowanie wartości
+                //przepisujemy posrotwane wartości z listy do pola textArea
+
+                try{
+                    List<Integer>intData = new ArrayList<>();
+                    for (String intElement : data) {
+                        intData.add(Integer.parseInt(intElement));
+                    }
+
+                    bubbleSortGeneric(intData);
+
+                    data.clear();
+                    for (Integer intDatum : intData) {
+                        data.add(String.valueOf(intDatum));
+                    }
+                    dataArea.setText(String.join("\n", data));
+                    JOptionPane.showMessageDialog(this,"Pomyślnie posortowano dane!");
+                }catch (NumberFormatException ex){
+                    JOptionPane.showMessageDialog(this, "Wybrano nieprawidłowy typ danych!");
+                }
+            }
+            else if (type.equals("Liczby zmiennoprzecinkowe")) {
+
+                try{
+                    List<Double>doubleData = new ArrayList<>();
+                    for (String doubleElement : data) {
+                        doubleData.add(Double.parseDouble(doubleElement.replace(',','.')));
+                    }
+                    insertionSort(doubleData);
+                    data.clear();
+                    for (Double doubleDatum : doubleData) {
+                        data.add(String.valueOf(doubleDatum));
+                    }
+                    dataArea.setText(String.join("\n", data));
+                    JOptionPane.showMessageDialog(this,"Pomyślnie posortowano dane!");
+                }catch (NumberFormatException ex){
+                    JOptionPane.showMessageDialog(this, "Wybrano nieprawidłowy typ danych!");
+                }
+
+            }
+            else {
+                try{
+                    bubbleSortGeneric(data);
+                    dataArea.setText(String.join("\n", data));
+                    JOptionPane.showMessageDialog(this,"Pomyślnie posortowano dane!");
+                }catch (NumberFormatException ex){
+                    JOptionPane.showMessageDialog(this, "Wybrano nieprawidłowy typ danych!");
+                }
+            }
+
+
+        });
+
+
+        //Zapisz do pliku - o podanej nazwie
+
+        //ewentulanie rozdzielać zpisywanie posortowanych danych i zmieniać pod tym względem nazwę zpisywanego pliku
+
+        saveToFileButton.addActionListener(e -> {
+            if(fileField.getText().isEmpty()){
+                JOptionPane.showMessageDialog(this,"Brak danych do zapisania!");
+            }
+            try(BufferedWriter bw = new BufferedWriter(new FileWriter("sorted_"+fileField.getText()+".txt"))){
+                for (String s : data) {
+                    bw.write(s);
+                    bw.newLine();
+                }
+                JOptionPane.showMessageDialog(this, "Dane zostały zapisane do pliku sorted_"+fileField.getText()+".txt!");
+            }catch (IOException ex) {
+                JOptionPane.showMessageDialog(this, "Błąd podczas zapisu do pliku: " + ex.getMessage());
+            }
+        });
+
+
+
     }
+    //Metoda generująca losowe ciągi znaków
     public String generateRandomString(Random random, int length) {
         String characters = "abcdefghijklmnopqrstuvwxyz";
         StringBuilder sb = new StringBuilder(length);
@@ -164,6 +283,50 @@ public class Frame extends JFrame {
             sb.append(characters.charAt(random.nextInt(characters.length())));
         }
         return sb.toString();
+    }
+    //robimy metode generyczną albo przeciążone werjsie tej samej metody z innym typem wejścia
+    public <T extends Comparable<T>>void bubbleSortGeneric(List<T> list) {
+        int n = list.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j).compareTo(list.get(j + 1)) > 0) {
+                    T temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+    public <T extends Comparable<T>>void insertionSort(List<T> list) {
+        int n = list.size();
+        for (int i = 1; i < n; i++) {
+            T temp = list.get(i);
+            int j = i - 1;
+            while (j >= 0 && list.get(j).compareTo(temp) > 0) {
+                list.set(j + 1, list.get(j));
+                j--;
+            }
+            list.set(j + 1, temp);
+        }
+    }
+    //Własne wersje metod tryParse z C# bo w Javie nie widze czegoś takiego
+    public static boolean tryParseInt(String input){
+     try {
+         int value = Integer.parseInt(input);
+         return true;
+     }catch (NumberFormatException ex){
+         return false;
+     }
+    }
+
+    public static boolean tryParseDouble(String input){
+        try {
+            double value = Double.parseDouble(input);
+            return true;
+        }catch (NumberFormatException ex){
+            return false;
+        }
     }
 
 }
